85169132aa35b38c76968d8716e0f1c939c006d3
Shivaya007 <shivayagupta@gmail.com>
2025-02-25 00:21:30 +0530

    changes

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -9,7 +9,7 @@ export default function Login(){
   let navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/loginuser", {
+    const response = await fetch("${process.env.REACT_APP_BACKEND_URL}/api/loginuser", {
       method: 'POST',
       headers: {
         'content-type': 'application/json'
